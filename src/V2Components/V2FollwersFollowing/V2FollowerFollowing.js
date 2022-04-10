725549febfc2478c3876a1ee094f15cf2e4a9019
김정민 <jungmini0601@gmail.com>
2022-04-10 18:22:45 +0900

    최종 수정

--- src/V2Components/V2FollwersFollowing/V2FollowerFollowing.js
+++ src/V2Components/V2FollwersFollowing/V2FollowerFollowing.js
@@ -1,5 +1,9 @@
-import React from 'react';
-import { List, Avatar,Button,Row,Col, Divider } from 'antd';
+import React,{useState} from 'react';
+import { List, Avatar,Button,Row, Divider, Typography,Collapse  } from 'antd';
+import { DownOutlined,UpOutlined } from '@ant-design/icons';
+
+const { Title } = Typography;
+const { Panel } = Collapse;
 
 function V2FollowerFollowing() {
   const data = [
@@ -16,27 +20,65 @@ function V2FollowerFollowing() {
     title: 'Ant Design Title 4',
   },
 ];
-  
+  const [followerVisible, setfollowerVisible] = useState(false);
+
+  const onFollowerVisibleHanlder = () => {
+    setfollowerVisible(!followerVisible);
+  }
+
+  // 이따 지울거 
+  function callback(key) {
+  console.log(key);
+}
+
   return (
     <div>
-       <Row justify='space-around'>
-         <Col span={8} ><Button style={{width:"100%"}}>팔로워</Button></Col>
-         <Col span={8}><Button style={{width:"100%"}}>팔로잉</Button></Col>
+       <Row style={{backgroundColor:"#126889"}}>
+        <Title level={5} style={{color:"white"}}>팔로워</Title>
+       </Row>
+       <Row style={{backgroundColor:"#126889"}}>
+          <Title level={5} style={{paddingLeft:"15px", color:"white"}}>10명</Title>
         </Row>
-        <Divider style={{marginBottom:"0px"}}/>
-      <List
-        itemLayout="horizontal"
-        dataSource={data}
-        renderItem={item => (
-        <List.Item>
-          <List.Item.Meta
-            avatar={<Avatar src="https://joeschmoe.io/api/v1/random" size={64} style={{marginRight:"10px"}}/>}
-            title={<div style={{position:"relative"}}><a href="https://ant.design">이름</a><Button style={{position:"absolute", right: "0px", top:"50%", transform:"translate(-50%,0)"}}>팔로우</Button></div>}
-            description="자기소개 블라 블라 블라 블라 "
-          />
-        </List.Item>
-    )}
-  />
+        <Collapse defaultActiveKey={['1']} onChange={callback} style={{border:"none", backgroundColor:"#126889"}} 
+        expandIcon={({ isActive }) => <DownOutlined rotate={isActive ? 180 : 0} />}>
+          <Panel style={{backgroundColor:"#126889"}}>
+            <List
+          itemLayout="horizontal"
+          dataSource={data}
+          style={{ backgroundColor:"#126889"}}
+          renderItem={item => (
+            <List.Item>
+              <List.Item.Meta
+                avatar={<Avatar src="https://joeschmoe.io/api/v1/random" size={64} style={{marginRight:"10px"}}/>}
+                title={<div style={{position:"relative"}}><a href="https://ant.design" style={{color:"white"}}>이름</a><Button style={{position:"absolute", right: "0px", top:"50%", transform:"translate(-50%,0)"}}>팔로우</Button></div>}
+                description={<div style={{color:"white"}}>뭘봐이자식아</div>}
+              />
+            </List.Item>)}/>    
+          </Panel>
+        </Collapse>
+        {/* 여기가 팔로잉 */}
+        <Row>
+        <Title level={5}>팔로잉</Title>
+       </Row>
+       <Row>
+          <Title level={5} style={{paddingLeft:"15px"}}>10명</Title>
+        </Row>
+        <Collapse defaultActiveKey={['1']} onChange={callback} style={{border:"none"}} expandIcon={({ isActive }) => <DownOutlined rotate={isActive ? 180 : 0} />}>
+          <Panel>
+            <List
+          itemLayout="horizontal"
+          dataSource={data}
+          style={{transition:"all 2s"}}
+          renderItem={item => (
+            <List.Item>
+              <List.Item.Meta
+                avatar={<Avatar src="https://joeschmoe.io/api/v1/random" size={64} style={{marginRight:"10px"}}/>}
+                title={<div style={{position:"relative"}}><a href="https://ant.design">이름</a><Button style={{position:"absolute", right: "0px", top:"50%", transform:"translate(-50%,0)"}}>팔로우</Button></div>}
+                description="자기소개 블라 블라 블라 블라 "
+              />
+            </List.Item>)}/>    
+          </Panel>
+        </Collapse>
     </div>
   )
 }
